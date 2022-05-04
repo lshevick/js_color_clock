589571047d58dcc26a0164fc7321502abbcf8527
Levi Shevick <levidshevick@gmail.com>
2022-05-04 13:09:49 -0400

    adds function to log time every second

--- main.js
+++ main.js
@@ -2,8 +2,17 @@
 // 1. create a time variable to use in further functions 
 //2. create function to interact with the DOM and be able to
 // 3. change the clock on the HTML page
-const today = new Date();
-const time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
 
+let updateTime = () => {
+    const today = new Date();
+    const time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+    console.log(time);
+}
+updateTime(); // logs current time on page load
+
+// logs current time once every second
+setInterval(function() {
+    updateTime();
+}, 1000);
 
 })();
