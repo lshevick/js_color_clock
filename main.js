7fea7e1b0a38a03c9bec028bab21fe051680d3b3
Levi Shevick <levidshevick@gmail.com>
2022-05-04 15:14:16 -0400

    adds hex conversions for time and background color changes

--- main.js
+++ main.js
@@ -6,6 +6,7 @@
 const clockBar = document.querySelector('.clock-progress-bar'); 
 const clockDisplay = document.querySelector('.clock-display');
 const clockFace = document.querySelector('.clock-display');
+const clock = document.querySelector('.clock');
 
 
 
@@ -23,15 +24,40 @@ let updateTime = () => {
     }
     const time = hours + ':' + minutes + ':' + seconds;
     clockDisplay.innerHTML = time;
-    console.log(time);
+    // console.log(time);
     
     // function to convert seconds of a minute to a percentage
     let percentSecs = Math.floor((seconds / 60) * 100);
-    console.log(percentSecs);
+    // console.log(percentSecs);
     
+    //updates clock bar with percentage of seconds in a minute
     clockBar.style.width = percentSecs + '%';
     clockBar.style.transition = 'all 0.2s ease-in-out';
+
+
+    // console.log() the time in hexidecimal format
     
+    let hexHours = parseInt(hours * 255 / 23).toString(16);
+    let hexMinutes = parseInt(minutes * 255 / 59).toString(16);
+    let hexSecs = parseInt(seconds * 255 / 59).toString(16);
+
+
+    let hexTime = `#${hexHours}${hexMinutes}${hexSecs}`;
+    
+
+    
+    console.log(hexTime);
+    
+    clock.style.backgroundColor = hexTime;
+
+    // hover over time shows Hex code
+    clockDisplay.addEventListener('mouseover', function(e) {
+
+        setTimeout(function() {
+        e.target.style.backgroundColor = '';
+        }, 500)
+    })
+
 
 }
 updateTime(); // logs current time on page load & updates clock
