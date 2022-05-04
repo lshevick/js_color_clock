506bdbca002128b6caa7452101d3beb8fe440f5c
Levi Shevick <levidshevick@gmail.com>
2022-05-04 12:51:10 -0400

    adds date and time varirables

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 (function (){
+// 1. create a time variable to use in further functions 
+//2. create function to interact with the DOM and be able to
+// 3. change the clock on the HTML page
+const today = new Date();
+const time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
 
-    
+
 })();
