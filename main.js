2544f3f38693a90ed16de3bb16a51d418640e2ed
Levi Shevick <levidshevick@gmail.com>
2022-05-04 13:35:46 -0400

    adds DOM elements & updating HTML clock

--- main.js
+++ main.js
@@ -2,17 +2,27 @@
 // 1. create a time variable to use in further functions 
 //2. create function to interact with the DOM and be able to
 // 3. change the clock on the HTML page
+const clockBar = document.querySelector('.clock-progress-bar'); 
+const clockDisplay = document.querySelector('.clock-display');
+const clockFace = document.querySelector('.clock-display');
+
+const today = new Date();
+const time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+
 
 let updateTime = () => {
     const today = new Date();
     const time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+    clockDisplay.innerHTML = time;
     console.log(time);
 }
-updateTime(); // logs current time on page load
+updateTime(); // logs current time on page load & updates clock
 
 // logs current time once every second
 setInterval(function() {
     updateTime();
 }, 1000);
+
+
 
 })();
